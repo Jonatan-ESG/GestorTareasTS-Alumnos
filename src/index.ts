6f2e0021689f0f3f0ac333b9cfab10b320117b2d
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2025-05-10 14:49:37 -0600

    :construction_worker: Se agregó el método que permite marcar una tarea como completada

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,18 @@ class GestorTareas {
         }
         this.almacenTareas.push(nuevaTarea)
     }
+
+    completarTarea(titulo: string) {
+        const indiceTarea = this.almacenTareas.findIndex((tarea: Tarea) => tarea.titulo == titulo)
+        if (indiceTarea == -1) return console.log('No fue posible encontrar la tarea indicada')
+        const tarea = this.almacenTareas[indiceTarea]
+        tarea.completada = true
+        tarea.estado = 'Completada'
+        this.almacenTareas[indiceTarea] = tarea
+    }
 }
 
 const store = new GestorTareas()
 store.agregarTarea('Limpiar el balcon', 'Limpiar el balcon bien')
+store.completarTarea('Limpia el balcon')
+console.log(store.almacenTareas)
